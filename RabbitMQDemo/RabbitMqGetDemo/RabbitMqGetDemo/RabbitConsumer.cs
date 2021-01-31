3dcee70b55dcad085020f0dbdac8e31bdd48f727
alexheiming0504 <alexheiming@t-online.de>
2021-01-31 23:43:23 +0100

    Added RabbitMQ push/pull projects

---
+++ RabbitMQDemo/RabbitMqGetDemo/RabbitMqGetDemo/RabbitConsumer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqGetDemo
+{
+    public class RabbitConsumer : BackgroundService
+    {
+        public RabbitConsumer()
+        {
+            InitializeRabbitConsumer();
+        }
+        IModel Channel;
+        private void InitializeRabbitConsumer()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            var connection = factory.CreateConnection();
+            Channel = connection.CreateModel();
+            Channel.QueueDeclare(
+                queue: "rabbitForApi",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(Channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                System.Diagnostics.Debug.WriteLine(content);
+                Channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            Channel.BasicConsume("rabbitForApi", false, consumer);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RabbitMQDemo/RabbitMqPushDemo/RabbitMqPushDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RabbitMqPushDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Init RabbitMQ...");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "rabbitForApi",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello World at " + DateTime.Now + "!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "rabbitForApi",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine("Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
